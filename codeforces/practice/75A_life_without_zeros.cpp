c475f0a961763c4dafc045cea949f4126191e636
carnivorous-hotdog <makkoncept.cp@gmail.com>
2020-03-09 22:54:41 +0530

    add 75A_life_without_zeros.cpp

---
+++ codeforces/practice/75A_life_without_zeros.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define vi vector<int>
+#define vii vector<int, int>
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+#ifdef LOCAL
+#define trace(...) __f(#__VA_ARGS__, __VA_ARGS__)
+template <typename Arg1>
+void __f(const char* name, Arg1&& arg1){
+  cerr << name << ": " << arg1 << endl;
+}
+template <typename Arg1, typename... Args>
+void __f(const char* names, Arg1&& arg1, Args&&... args){
+  const char* comma = strchr(names + 1, ',');
+  cerr.write(names, comma - names) << ": " << arg1 << " |";
+  __f(comma + 1, args...);
+}
+#else
+#define trace(...) 42
+#endif
+
+int main(){
+    IOS;
+    #ifdef LOCAL
+        //freopen("input.txt", "r", stdin);
+        //freopen("output.txt", "w", stdout);
+    #endif
+
+    string a, b;
+    cin >> a >> b;
+
+    string c = to_string(stoi(a) + stoi(b));
+
+    string aa = "", bb = "", ccc = "";
+
+    for(auto x: a){
+        if (x != '0') aa += x;
+    }
+
+    for(auto x: b){
+        if (x != '0') bb += x;
+    }
+
+    for (auto x: c){
+        if (x != '0') ccc += x;
+    }
+
+    string cc = to_string(stoi(aa) + stoi(bb));
+
+    cout << (ccc == cc ? "YES": "NO") << endl;
+
+    #ifdef LOCAL
+        cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+    #endif
+
+    return 0;
+}
+
+
