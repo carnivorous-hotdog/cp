0e9dea944a3c195a87b7c3adc7bc599cc1f76d72
carnivorous-hotdog <makkoncept.cp@gmail.com>
2020-04-03 11:48:28 +0530

    add FUNRUN.cpp

---
+++ codechef/contest/CHPTRS01/FUNRUN.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define vi vector<int>
+#define vii vector<int, int>
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+#ifdef LOCAL
+#define trace(...) __f(#__VA_ARGS__, __VA_ARGS__)
+template <typename Arg1>
+void __f(const char* name, Arg1&& arg1){
+  cerr << name << ": " << arg1 << endl;
+}
+template <typename Arg1, typename... Args>
+void __f(const char* names, Arg1&& arg1, Args&&... args){
+  const char* comma = strchr(names + 1, ',');
+  cerr.write(names, comma - names) << ": " << arg1 << " |";
+  __f(comma + 1, args...);
+}
+#else
+#define trace(...) 42
+#endif
+
+int main(){
+    IOS;
+    #ifdef LOCAL
+        //freopen("input.txt", "r", stdin);
+        //freopen("output.txt", "w", stdout);
+    #endif
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        ll a, b;
+        a = b = 0;
+
+        for (int i = 0; i < n; i++){
+            ll x;
+            cin >> x;
+            a = max(a, x);
+        }
+
+        for (int i = 0; i < n; i++){
+            ll x;
+            cin >> x;
+            b = max(b, x);
+        }
+
+        cout << (abs(b - a) >= 1 ? "YES" : "NO" ) << endl;
+    }
+
+
+
+    #ifdef LOCAL
+        cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+    #endif
+
+    return 0;
+}
+
+
