6f9a4521e5e19ef3502fcfcb92dccedaef2d5497
carnivorous-hotdog <makkoncept.cp@gmail.com>
2020-04-04 07:28:48 +0530

    add CARSELL.cpp COVIDLQ.cpp

---
+++ codechef/contest/APRIL20B/COVIDLQ.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define vi vector<int>
+#define vii vector<int, int>
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+#ifdef LOCAL
+#define trace(...) __f(#__VA_ARGS__, __VA_ARGS__)
+template <typename Arg1>
+void __f(const char* name, Arg1&& arg1){
+  cerr << name << ": " << arg1 << endl;
+}
+template <typename Arg1, typename... Args>
+void __f(const char* names, Arg1&& arg1, Args&&... args){
+  const char* comma = strchr(names + 1, ',');
+  cerr.write(names, comma - names) << ": " << arg1 << " |";
+  __f(comma + 1, args...);
+}
+#else
+#define trace(...) 42
+#endif
+
+int main(){
+    IOS;
+    #ifdef LOCAL
+        //freopen("input.txt", "r", stdin);
+        //freopen("output.txt", "w", stdout);
+    #endif
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+
+        int a[n];
+
+        bool ok = true;
+        int lst = -1;
+        for (int i = 0; i < n; i++){
+            cin >> a[i];
+            if (a[i] == 1){
+                if (lst == -1){
+                    lst = i;
+                } else {
+                    if (i - lst < 6){
+                        ok = false;
+                    }
+                    lst = i;
+                }
+            }
+        }
+
+        cout << (ok == true ? "YES" : "NO") << endl;
+    }
+
+
+
+    #ifdef LOCAL
+        cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+    #endif
+
+    return 0;
+}
+
+
