35254afda2cc94f1d978a4fa82bb3970ba012c63
carnivorous-hotdog <makkoncept.cp@gmail.com>
2020-04-24 08:44:15 +0530

    add 4 problems

---
+++ csacademy/practice/plants.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define vi vector<int>
+#define vii vector<int, int>
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define endl '\n'
+#define idk ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+#ifdef LOCAL
+#define trace(...) __f(#__VA_ARGS__, __VA_ARGS__)
+template <typename Arg1>void __f(const char* name, Arg1&& arg1){cout<<name<<" : "<<arg1<<"\n";}
+template <typename Arg1, typename... Args>void __f(const char* names, Arg1&& arg1, Args&&... args)
+{const char* comma=strchr(names+1,',');cout.write(names,comma-names)<<" : "<<arg1<<" |";__f(comma+1, args...);} 
+#define traceloop(x,a) { cerr<<#a<<": "; for(ll ix=0;ix<x;ix++) cerr<<a[ix]<<" "; cerr<<endl; }
+
+#else
+#define trace(...) 42
+#define traceloop(...) 42
+#endif
+const int MOD=1e9+7;
+
+//////////////////////// code starts ///////////////////////
+
+int main(){
+    idk
+    #ifdef LOCAL
+        //freopen("input.txt", "r", stdin);
+        //freopen("output.txt", "w", stdout);
+    #endif
+
+    int n, m;
+    cin >> n >> m;
+
+    int temp[101] = {0};
+
+    for (int i = 1; i <= m; i++){
+        int x;
+        cin >> x;
+        temp[x]++;
+    }
+
+    for (int i = 1; i <= 100; i++){
+        temp[i] += temp[i-1];
+    }
+
+    while(n--){
+        int a, b;
+        cin >> a >> b;
+        if (a == 0) cout << temp[b] << endl;
+        else cout << temp[b] - temp[a-1] << endl;
+    }
+
+    #ifdef LOCAL
+        cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+    #endif
+
+    return 0;
+}
+
+
