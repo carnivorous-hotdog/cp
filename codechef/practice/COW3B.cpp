62037f5933298b1f29980fd86762999848b48e03
carnivorous-hotdog <makkoncept.cp@gmail.com>
2020-05-03 08:31:34 +0530

    add 2 problems

---
+++ codechef/practice/COW3B.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define vi vector<int>
+#define vii vector<pair<int, int>>
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define endl '\n'
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+#ifdef LOCAL
+#define trace(...) __f(#__VA_ARGS__, __VA_ARGS__)
+template <typename Arg1>void __f(const char* name, Arg1&& arg1){cerr<<name<<" : "<<arg1<<"\n";}
+template <typename Arg1, typename... Args>void __f(const char* names, Arg1&& arg1, Args&&... args)
+{const char* comma=strchr(names+1,',');cerr.write(names,comma-names)<<" : "<<arg1<<" |";__f(comma+1, args...);} 
+#define traceloop(x,a) { cerr<<#a<<": "; for(ll ix=0;ix<x;ix++) cerr<<a[ix]<<" "; cerr<<endl; }
+
+#else
+#define trace(...) 42
+#define traceloop(...) 42
+#endif
+const int MOD=1e9+7;
+
+//////////////////////// code starts ///////////////////////
+
+int main(){
+    IOS;
+    #ifdef LOCAL
+        //freopen("input.txt", "r", stdin);
+        //freopen("output.txt", "w", stdout);
+    #endif
+
+    int t;
+    cin >> t;
+    while(t--){
+        int n, m;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++) cin >> a[i];
+
+        set<int> b;
+        cin >> m;
+
+        for (int i = 0; i < m; i++){
+            int x;
+            cin >> x;
+            b.insert(x);
+        }
+
+        bool f = 0;
+        int ans = 0;
+        for (int i = 0; i < n; i++){
+            if (b.count(a[i])){
+                f = 1;
+            } 
+            else {
+                if (f){
+                    ans++;
+                }
+                f = 0;
+            }
+        }
+
+        if (f) ans++;
+
+        cout << ans << endl;
+    }
+
+
+
+    #ifdef LOCAL
+        cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+    #endif
+
+    return 0;
+}
+
+
